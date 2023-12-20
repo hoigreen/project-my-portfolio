539c90a6bdc5808fd1e5c35d26554a3e7e49e8ed
HoiGreen <hoitrnd107@gmail.com>
2023-12-20 09:22:01 +0700

    Complete Ui of Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,26 @@
+'use client'
+
+import { useEffect, useState } from "react"
 import Logo from "./nav/Logo"
 import MobileNavbar from "./nav/MobileNavbar"
 import Navbar from "./nav/Navbar"
 import { ThemeToggler } from "./theme/ThemeToggler"
+import { usePathname } from "next/navigation"
 
 const Header = () => {
+  const [header, setHeader] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const scrollPos = () => window.scrollY >= 50 ? setHeader(true) : setHeader(false)
+
+    window.addEventListener("scroll", scrollPos)
+
+    return () => window.removeEventListener('scroll', scrollPos)
+  }, [])
+
   return (
-    <header>
+    <header className={`${header ? 'py-4 bg-white shadow-lg dark:bg-accent' : 'py-6 dark: bg-transparent'} sticky top-0 z-30 transition-all ${pathname === '/' && 'bg-[#c1ebc529]'}`}>
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <Logo />
@@ -13,8 +28,8 @@ const Header = () => {
           <div className="flex items-center gap-x-6">
             <Navbar
               containerStyles="hidden xl:flex gap-x-8 items-center"
-              linkStyles=""
-              underlineStyles=""
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full rounded-full"
             />
             <ThemeToggler />
             <div className="xl:hidden">
