7cce252ef8392331cc01276143d74f3fcc8f677d
HoiGreen <hoitrnd107@gmail.com>
2023-12-20 08:58:11 +0700

    Add image in Logo.tsx

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <Link href='/'>
+      <Image
+        src='/logo.svg'
+        width={54}
+        height={54}
+        alt='Logo of my portfolio page'
+      />
+    </Link>
   )
 }
 
