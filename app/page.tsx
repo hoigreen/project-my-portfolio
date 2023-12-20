573ac0fe076587dd6f2e893e70dddf87376ab987
HoiGreen <hoitrnd107@gmail.com>
2023-12-20 09:53:52 +0700

    Add componens nessecery for homepage at page.tsx in /app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Image from 'next/image'
+import { RiArrowDownSLine } from 'react-icons/ri'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Trang chu</main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section>
+        <div className="container mx-auto">
+          <div>
+            Text
+          </div>
+
+          <div>Text</div>
+
+          <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
+            <RiArrowDownSLine className='text-3xl text-primary' />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
