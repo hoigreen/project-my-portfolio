70f7c8da6038a705ef90cbe8f6622158210e480a
HoiGreen <hoitrnd107@gmail.com>
2023-12-21 15:54:04 +0700

    feat: complete MobileNavbar component

--- components/nav/MobileNavbar.tsx
+++ components/nav/MobileNavbar.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { AlignJustify } from 'lucide-react'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import Social from '../home/Social'
 
 const MobileNavbar = () => {
   return (
-    <div>MobileNavbar</div>
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className='cursor-pointer' />
+      </SheetTrigger>
+      <SheetContent>
+        <div className='flex flex-col justify-between h-full '>
+          <div className='flex flex-col items-center gap-y-8'>
+            <Logo />
+            <Navbar
+              containerStyles='flex w-full flex-col items-end gap-y-6'
+              linkStyles='text-lg py-4 w-full text-right'
+            />
+          </div>
+          <Social
+            containerStyles='flex justify-center gap-x-4'
+            iconStyles='text-2xl'
+          />
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
