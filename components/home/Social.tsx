93e029ba9b16f5379e2eee3c97537610b9563a08
HoiGreen <hoitrnd107@gmail.com>
2023-12-21 10:48:38 +0700

    feat: complete build component Social

--- components/home/Social.tsx
+++ components/home/Social.tsx
@@ -1,8 +1,45 @@
+'use client'
+import Link from 'next/link'
 import React from 'react'
+import { RiFacebookFill, RiGithubFill, RiInstagramFill, RiLinkedinFill, RiYoutubeFill } from 'react-icons/ri'
 
-const Social = () => {
+interface SocialProps {
+  containerStyles: string,
+  iconStyles: string
+}
+
+const icons = [
+  {
+    path: '/',
+    name: <RiFacebookFill />
+  },
+  {
+    path: '/',
+    name: <RiLinkedinFill />
+  },
+  {
+    path: '/',
+    name: <RiGithubFill />
+  },
+  {
+    path: '/',
+    name: <RiYoutubeFill />
+  },
+  {
+    path: '/',
+    name: <RiInstagramFill />
+  }
+]
+
+const Social = ({ containerStyles, iconStyles }: SocialProps) => {
   return (
-    <div>Social</div>
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, i) =>
+        <Link key={i} href={icon.path} target="_blank">
+          <div className={`${iconStyles}`}>{icon.name}</div>
+        </Link>
+      )}
+    </div>
   )
 }
 
