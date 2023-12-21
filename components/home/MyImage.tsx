b081c9e7dcf69e6946d397d5c60acb88faff6f79
HoiGreen <hoitrnd107@gmail.com>
2023-12-21 10:49:01 +0700

    feat: complete build component MyImage

--- components/home/MyImage.tsx
+++ components/home/MyImage.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
-const MyImage = () => {
+interface MyImageProps {
+  containerStyle: string,
+  imageSrc: string
+}
+
+const MyImage = (props: MyImageProps) => {
+  const { containerStyle, imageSrc } = props
   return (
-    <div>MyImage</div>
+    <div className={`overflow-hidden ${containerStyle}`}>
+      <Image src={imageSrc} fill priority className="z-50" alt='hoigreen' />
+    </div>
   )
 }
 
