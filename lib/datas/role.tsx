0beb63f6c9e14a8252b2a07bc091730a39b6b7ce
HoiGreen <hoitrnd107@gmail.com>
2023-12-22 20:14:44 +0700

    feat: add new data of my roles

---
+++ lib/datas/role.tsx
@@ -0,0 +1,18 @@
+import { Cog, MonitorCheck } from "lucide-react";
+
+const roleData = [
+  {
+    icon: <MonitorCheck size={80} strokeWidth={0.8} />,
+    title: 'Frontend Web Developer',
+    description: 'Experienced Frontend Developer with expertise in ReactJS and Next.js. I specialize in creating seamless, user-friendly web applications. Let\'s build something amazing together! 🚀 #ReactJS #NextJS #WebDevelopment'
+  },
+  {
+    icon: <Cog size={80} strokeWidth={0.8} />,
+    title: 'Backend Developer',
+    description: 'Backend Developer specializing in Node.js and .NET Framework. Proficient in building scalable and efficient server-side solutions. Let\'s power your applications with robust backend architecture! 💻 #NodeJS #DotNET #BackendDevelopment'
+  }
+]
+
+export {
+  roleData
+}
