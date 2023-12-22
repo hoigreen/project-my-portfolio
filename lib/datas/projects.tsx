3d7350408417780b2895202908bb668f0e7b472b
HoiGreen <hoitrnd107@gmail.com>
2023-12-22 21:08:02 +0700

    feat: add new data of my projects

---
+++ lib/datas/projects.tsx
@@ -0,0 +1,45 @@
+const projectsData = [
+  {
+    image: '/project/1.png',
+    category: 'ReactJS + NodeJS',
+    name: 'ShopTECH',
+    description: 'This is an e-commerce website that allows users to order cakes available on the website. Admin can manage orders, product quantity, buyer information, promotion information and customer information, as well as revenue statistics. On the customer side, customers can view product information, promotions, make purchases and make product reviews.',
+    technology: [
+      "Using ReactJS, CSS, SCSS module to build front-end for website.",
+      "Using MongoDB to storage database.",
+      "Using NodeJS and framework ExpressJS to con¦gure for server on website."
+    ],
+    link: "https://shoptech.website/",
+    github: 'https://github.com/hoigreen/project-ecommerce-shop'
+  },
+  {
+    image: '/project/2.png',
+    category: '.NET Framework',
+    name: 'Tinh ban Coffee',
+    description: 'The Coffee Shop Management project is a comprehensive software solution designed to streamline and enhance the management of a coffee shop. Built using .NET WinForms, C#, Guna Framework, and integrated with SQL Server',
+    technology: [
+      "Programming Language: C#.",
+      "User Interface Framework: .NET WinForms, Guna Framework, LiveCharts.",
+      "Database: Mircosoft SQL Server."
+    ],
+    link: "/",
+    github: 'https://github.com/hoigreen/project-coffee-shop-management-ado-dotnet'
+  },
+  {
+    image: '/project/3.png',
+    category: 'ReactJS',
+    name: 'Real-time Bidding Product Website',
+    description: 'The website allows users to participate in realtime bidding for a speci¦c product between users.',
+    technology: [
+      " Using ReactJS, SCSS to build front-end.",
+      "Using JSON Server to store user and product data.",
+      "Using JavaScript, NodeJS to handle real-time events."
+    ],
+    link: "https://bidding-page.vercel.app/",
+    github: 'https://github.com/hoigreen/project-bidding'
+  },
+]
+
+export {
+  projectsData
+}
