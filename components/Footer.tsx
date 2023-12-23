f75847999bd99034356b5e8f86271983760fbfba
HoiGreen <hoitrnd107@gmail.com>
2023-12-23 12:05:55 +0700

    feat: complete Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,21 @@
+import Social from "./home/Social"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="pt-12">
+      <div className="container mx-auto relative">
+        <div className="flex flex-col items-center justify-between">
+          <Social
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-12"
+            iconStyles="text-primary text-[20px] hover:text-gray-700 dark:hover:text-white transition-all"
+          />
+
+          <div className="text-muted-foreground absolute bottom-0">
+            HoiGreen - Truong Quoc Hoi
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
