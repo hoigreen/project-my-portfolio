3a7d01bc53fb3de09bebdfdecee906ce7a999872
HoiGreen <hoitrnd107@gmail.com>
2023-12-23 11:55:49 +0700

    feat: modifiy component CTA

---
+++ components/home/Cta.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '../ui/button'
+import { Send } from 'lucide-react'
+
+const Cta = () => {
+  return (
+    <section className="py-24 bg-green-50 dark:bg-secondary">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center">
+          <h2 className="h2 text-center mb-12">
+            Ready to elevate your digital experience?
+          </h2>
+
+          <p className="subtitle text-center">Let's collaborate on innovative software solutions that drive results. Whether you have a specific project in mind or just want to explore the possibilities, I'm here to help. Contact me to start the conversation.
+          </p>
+
+          <Link href='/contact'>
+            <Button>Contact me for work <Send size={24} className='ml-2' /></Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Cta
