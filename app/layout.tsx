0301226d094a431fbf566503b5841528c21f60c9
HoiGreen <hoitrnd107@gmail.com>
2023-12-27 16:23:26 +0700

    style: format code layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,11 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children, params }: { children: React.ReactNode, params: { lang: string } }) => {
   return (
-    <html lang={params.lang ?? defaultLocale} suppressContentEditableWarning>
+    <html
+      lang={params.lang ?? defaultLocale}
+      suppressContentEditableWarning
+    >
+      <head />
       <body className={playpenSans.className}>
         <ThemeProvider attribute='class' defaultTheme='light'>
           <Header />
