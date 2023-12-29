44b8a240857722fc1a13498c9abec6efbab38a85
HoiGreen <hoitrnd107@gmail.com>
2023-12-29 08:49:40 +0700

    feat: complete translate locale 'vi' page route '/projects'

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -4,18 +4,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { projectsData } from '@/lib/datas/projects'
 import React, { useState } from 'react'
 
-const uniqueCategories = ['All Projects', ...new Set(projectsData.map((item: any) => item.category))];
+const uniqueCategories = ['All Projects', ...new Set(projectsData['en'].map((item: any) => item.category))];
 
-const MyProjects = () => {
+const MyProjects = ({ params: { locale } }: { params: { locale: string } }) => {
   const [categories, setCategories] = useState(uniqueCategories)
   const [currentCategory, setCurrentCategory] = useState('All Projects')
 
-  const filteredProjects = projectsData.filter(item => currentCategory === 'All Projects' ? item : item.category === currentCategory)
+  const filteredProjects = projectsData[locale].filter(item => currentCategory === 'All Projects' ? item : item.category === currentCategory)
 
   return (
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
-        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">My Projects</h2>
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">{locale === 'en' ? 'My Projects' : 'Dự án của tớ'}</h2>
 
         <Tabs defaultValue={currentCategory} className='mb-24 xl:mb-48'>
           <TabsList className='w-full grid h-full grid-cols-2 md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none '>
